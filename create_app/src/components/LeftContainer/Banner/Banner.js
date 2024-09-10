d39bcea0243207e7611ca3ad55148a3a8d7b632a
sehihi <tpgus8618@gmail.com>
2024-09-10 11:23:42 +0900

    2차 수정 완

---
+++ create_app/src/components/LeftContainer/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Banner.css';
+
+const Banner = () => {
+    return (
+      <div className="banner">
+        <h2>Banner</h2>
+      </div>
+    );
+  };
+
+export default Banner;
